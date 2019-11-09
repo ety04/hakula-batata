8d1601ad1467fcc51812ba415f317d40a3671fb5
Ety <eternal.raver@live.it>
2019-11-09 14:20:01 +0100

    added new file: index

---
+++ index.js
@@ -0,0 +1,22 @@
+/* ---------------------------- *
+* Hakula-Batata
+* ~~~~~
+* Hakula-Batata is a new generic bot for Discord servers.
+* ~~~~~
+* birthday-bot is free to use, modify and redistribute. If you mention the creator, it's better.
+* Creator: Ety
+* License: LGPL-3.0
+* Language: Javascript with Node.js
+* Additional libraries: Discord.js
+* Initial use: Ety's new Discord server
+ * ---------------------------- */
+
+/** -------------------- IMPORTS ----------------------- */
+
+/** ---------------- GLOBAL VARIABLES ------------------ */
+
+/** --------------- INTERNAL FUNCTIONS ----------------- */
+
+/** ----------------- DISCORD EVENTS ------------------- */
+
+console.log("------------ Bot started! -------------");
