36e1f7f931ce08018b71a6ad5476efdde92742e4
Ety <eternal.raver@live.it>
2019-11-10 13:46:47 +0100

    added welcome/leave message

--- index.js
+++ index.js
@@ -19,17 +19,65 @@ const Discord = require('discord.js')
 
 const bot = new Discord.Client();
 
+// Keys
 const token = process.env.TK_HB_BOT;
 
+// Channels
+const adm_cn = process.env.ADM_CN;
+const reglement_cn = process.env.RGM_CN;
+const roles_cn = process.env.ROL_CN;
+
+var canal_reglement, canal_roles, canal_admissions;
+
+// Roles
+const rentree_rl = process.env.RNT_RL;
+
+
 /** --------------- INTERNAL FUNCTIONS ----------------- */
+
+function createEmbedMessage(member, admission)
+{
+	let RE = new Discord.RichEmbed()
+		.setAuthor(member.user.username, member.user.displayAvatarURL)
+		.setFooter("La Direction");
+	if(admission)
+	{
+		RE.setColor("#4ee121")
+		  .setDescription("Bienvenue à " + member + " dans **La Récréation** ! Lis le " + canal_reglement + " et assigne tes rôles dans " + canal_roles ".");
+	}
+	else
+	{
+		RE.setColor("#e1215b")
+		.setDescription(member + " a quitté **La Récréation**. On lui souhaite une bonne continuation de sa scolarité.");
+	}
+	return RE;
+}
 
 /** ----------------- DISCORD EVENTS ------------------- */
 
 console.log("------------ Bot started! -------------");
 
 bot.on('ready', () => {
-	console.log("~~~~~~~~Bot connected!~~~~~~~~");
+	console.log("~~~~~~~~ Bot connected! ~~~~~~~~");
+	canal_reglement = bot.channels.get(reglement_cn);
+	canal_roles = bot.channels.get(roles_cn);
+	canal_admissions = bot.channels.get(adm_cn);
 });
 	
+bot.on('guildMemberAdd',  member => {
+		console.log('New member - ' + member.user.username);
+		
+		let joinRE = createEmbedMessage(member, true);
+		canal_admissions.send(joinRE);
+		member.addRole(rentree_rl);
+});
+
+bon.on('guildMemberRemove', member => {
+		console.log('Member leaving - ' + member.user.username);
+		
+		let leaveRE = createEmbedMessage(member, false);
+		canal_admissions.send(leaveRE);
+});
+
 /** ----------------- CONNECTION ------------------- */
 bot.login(token);
